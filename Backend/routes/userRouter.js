e5455a857d8b38fbae49ce5887da83e1aacebaee
Manoj N <manoj8112014@gmail.com>
2024-11-27 04:53:10 +0530

    initial commit

---
+++ Backend/routes/userRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { register,login,logout,getUser ,updateProfile } from "../controllers/userController.js";
+import { isAuthenticated } from "../middlewares/auth.js"
+
+
+const router = express.Router();
+
+router.post("/register",register);
+router.post("/login",login);
+//router.get("/logout",logout);
+router.get("/logout",isAuthenticated,logout);
+router.get("/me",isAuthenticated,getUser);
+router.put("/update/profile",isAuthenticated,updateProfile);
+
+
+
+export default router;
